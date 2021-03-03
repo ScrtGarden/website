7cc9c07a9fc0bf399305678bec08eec18552eafa
hieulai <>
2021-03-03 22:12:51 +1100

    🏷️ extends DefaultTheme

--- styled.d.ts
+++ styled.d.ts
@@ -1,6 +1,9 @@
 // import original module declarations
-import 'styled-components';
+import 'styled-components'
+import theme from './src/styles/theme'
 
-declare module 'styled-compoents' {
-  
+type CustomTheme = typeof theme.light
+
+declare module 'styled-components' {
+  export interface DefaultTheme extends CustomTheme {}
 }
