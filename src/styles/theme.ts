a317ca6f04704924f369ee894e1b9dd2e6f94140
hieulai <>
2021-03-03 22:03:58 +1100

    💄 add themes

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,22 +1,89 @@
 const theme = {
   light: {
-    fontSizes: {
-      "xs": "1rem",
-      "sm": "1.2rem",
-      "md": "1.4rem",
-      "lg": "1.8rem",
-      "xl": "2.2rem",
-      "xxl": "2.6rem",
-      "xxxl": "3.6rem"
+    borderRadii: {
+      sm: '2px',
+      md: '4px',
     },
-    "palette": {
-      "black": "#000",
-      "white": "#fff",
-    }
+    fontSizes: {
+      xs: '1rem',
+      sm: '1.2rem',
+      md: '1.4rem',
+      lg: '1.8rem',
+      xl: '2.2rem',
+      xxl: '2.6rem',
+      xxxl: '3.6rem',
+    },
+    fontWeights: {
+      thin: 100,
+      extralight: 200,
+      light: 300,
+      regular: 400,
+      medium: 500,
+      semibold: 600,
+      bold: 700,
+      extrabold: 800,
+      black: 900,
+    },
+    lineHeights: {
+      sm: '1.6rem',
+      md: '2rem',
+      lg: '2.4rem',
+      xl: '2.8rem',
+      xxl: '3.2rem',
+      xxxl: '4.4rem',
+    },
+    iconSizes: {
+      sm: '1.2rem',
+      md: '1.6rem',
+      lg: '2.6rem',
+    },
+    space: {
+      base: 4,
+      xxs: '4px',
+      xs: '8px',
+      sm: '12px',
+      md: '20px',
+      lg: '32px',
+      xl: '40px',
+      xxl: '48px',
+    },
+    colors: {
+      font: {
+        brand: '#5eae91',
+        title: '#2f3941',
+        text: '#a0b3c6',
+        menuItem: '#f8f9f9',
+      },
+      border: '',
+      bg: '#12191d',
+      fg: '#e9ebed',
+      button: {
+        text: {
+          default: '#2f3941',
+          primary: '#2f3941',
+          secondary: '#2f3941',
+        },
+        background: {
+          default: {
+            base: '#f8f9f9',
+            hover: '#e9ebed',
+            active: '#d8dcde',
+          },
+          primary: {
+            base: '#499bfc',
+            hover: '#3c7fce',
+            active: '#326baf',
+          },
+          secondary: {
+            base: '#f0be72',
+            hover: '#d0a564',
+            active: '#af8b55',
+          },
+        },
+      },
+    },
   },
-  dark: {
-    //
-  }
+  dark: {},
 }
 
 export default theme
